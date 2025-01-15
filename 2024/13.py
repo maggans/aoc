aa1502a9a998bca5583068b4fb634ff5327596d2
unknown <markus.gerard@hotmail.com>
2025-01-15 22:51:36 +0100

    cleanup 2024:13-16

--- 2024/13.py
+++ 2024/13.py
@@ -1,53 +1,13 @@
 from aoc_utils import *
 
-res = 0
-
-# A - 3 tokens, B - 1 token
-g = getGroups(l)
-
-runs=[]
-runs2=[]
-for it in g:
-	a = it[0].split(": ")
-	a = a[1].split(", ")
-	ax = int(a[0].split("+")[1])
-	ay = int(a[1].split("+")[1])
-
-	b = it[1].split(": ")
-	b = b[1].split(", ")
-	bx = int(b[0].split("+")[1])
-	by = int(b[1].split("+")[1])
-	
-	p = "".join(it[2].split(": "))
-	p = p.split(", ")
-	px = int(p[0].split("=")[1])
-	py = int(p[1].split("=")[1])
-	
-	runs.append([ax,ay,bx,by,px,py])
-	runs2.append([ax,ay,bx,by,px+10000000000000,py+10000000000000])
-
-from z3 import *
-
-for i in range(len(runs)):
-	s = Solver()
-	x = Int("x")
-	y = Int("y")
-	s.add(x*runs[i][0] + y*runs[i][2] == runs[i][4])
-	s.add(x*runs[i][1] + y*runs[i][3] == runs[i][5])
-	if s.check() == sat:
-		m = s.model()
-		res+=int(str(m[x]))*3+int(str(m[y]))
-print("Part 1:",res)
-
-res = 0
-for i in range(len(runs2)):
-	s = Solver()
-	x = Int("x")
-	y = Int("y")
-	s.add(x*runs2[i][0] + y*runs2[i][2] == runs2[i][4])
-	s.add(x*runs2[i][1] + y*runs2[i][3] == runs2[i][5])
-	if s.check() == sat:
-		m = s.model()
-		res+=int(str(m[x]))*3+int(str(m[y]))
-
-print("Part 2:",res)
+res = [0,0]
+prize = [0,10000000000000]
+for A,B,P in getGroups(l):
+	ax,ay,bx,by,px,py = ints(A+B+P)
+	for i,dp,in enumerate(prize):
+		b = (ax*(py+dp) - ay*(px+dp)) / (ax*by - ay*bx)
+		a = ((px+dp) - b*bx) / ax
+		if a.is_integer() and b.is_integer():
+			res[i] += int(3*a+b)
+print("Part 1:",res[0])
+print("Part 2:",res[1])
--- 2024/16.py
+++ 2024/16.py
@@ -1,85 +1,38 @@
 from aoc_utils import *
 
-h = len(l)
-w = len(l[0])
-
-# S facing EAST
-
-sx = 0
-sy = 0
-ex = 0
-ey = 0
-for i in range(h):
+sx = sy = 0
+ex = ey = 0
+for i in range(len(l)):
 	l[i] = list(l[i])
-for i in range(h):
-	for j in range(w):
+	for j in range(len(l[i])):
 		if l[i][j] == "S":
-			sx = j
-			sy = i
-			l[i][j] = "."
+			sx,sy = j,i
 		if l[i][j] == "E":
-			ex = j
-			ey = i
-			l[i][j] = "."
-# print(sx,sy,ex,ey)
+			ex,ey = j,i
 
-q = []
-heapq.heappush(q,(0,sx,sy,"e",[]))
-
+q = [(0,sx,sy,0,[])]
 vis = {}
-
-paths = set()
-bestScore = -1
+paths = {(sx,sy)}
+bestScore = None
+dirs = [(1,0),(0,1),(-1,0),(0,-1)]
 while q:
-	next = heapq.heappop(q)
-	score,x,y,dir,path = next
+	score,x,y,dir,path = heapq.heappop(q)
 	
 	if x == ex and y == ey:
-		if bestScore == -1:
+		if bestScore is None or score == bestScore:
 			bestScore = score
-			paths = paths | set(path)
-			paths.add((x,y))
-		elif bestScore == score:
-			paths = paths | set(path)
-		# print(score)
-		# paths = paths | set(path)
-		# print("path: ",len(set(paths)))
+			paths |= set(path)
 		continue
 	
-	if (x,y,dir) in vis:
-		if vis[(x,y,dir)] != score:
+	if (x,y,dir) in vis and vis[(x,y,dir)] != score:
 			continue
 	vis[(x,y,dir)] = score
 	
-	if dir == "e":
-		if x+1 < w and l[y][x+1] == ".":
-			heapq.heappush(q,(score+1,x+1,y,dir,path+[(x,y)]))
-		if y-1 >= 0 and l[y-1][x] == ".":
-			heapq.heappush(q,(score+1000,x,y,"n",path+[(x,y)]))
-		if y+1 < h and l[y+1][x] == ".":
-			heapq.heappush(q,(score+1000,x,y,"s",path+[(x,y)]))
-		
-	elif dir == "w":
-		if x-1 >= 0 and l[y][x-1] == ".":
-			heapq.heappush(q,(score+1,x-1,y,dir,path+[(x,y)]))
-		if y-1 >= 0 and l[y-1][x] == ".":
-			heapq.heappush(q,(score+1000,x,y,"n",path+[(x,y)]))
-		if y+1 < h and l[y+1][x] == ".":
-			heapq.heappush(q,(score+1000,x,y,"s",path+[(x,y)]))
-	elif dir == "s":
-		if y+1 < h and l[y+1][x] == ".":
-			heapq.heappush(q,(score+1,x,y+1,dir,path+[(x,y)]))
-		if x-1 >= 0 and l[y][x-1] == ".":
-			heapq.heappush(q,(score+1000,x,y,"w",path+[(x,y)]))
-		if x+1 < w and l[y][x+1] == ".":
-			heapq.heappush(q,(score+1000,x,y,"e",path+[(x,y)]))
-	elif dir == "n":
-		if y-1 >= 0 and l[y-1][x] == ".":
-			heapq.heappush(q,(score+1,x,y-1,dir,path+[(x,y)]))
-		if x-1 >= 0 and l[y][x-1] == ".":
-			heapq.heappush(q,(score+1000,x,y,"w",path+[(x,y)]))
-		if x+1 < w and l[y][x+1] == ".":
-			heapq.heappush(q,(score+1000,x,y,"e",path+[(x,y)]))
+	dx,dy = dirs[dir]
+	if l[y+dy][x+dx] != "#":
+		heapq.heappush(q,(score+1,x+dx,y+dy,dir,path+[(x+dx,y+dy)]))
+	heapq.heappush(q,(score+1000,x,y,(dir+1)%4,path))
+	heapq.heappush(q,(score+1000,x,y,(dir+3)%4,path))
 
 print("Part 1:", bestScore)
 print("Part 2:", len(paths))
